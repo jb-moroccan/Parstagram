ba12dffe776f1b4b0dddc9a7cc2d2a6153816db2
jb-moroccan <jbousellam@yahoo.com>
2020-10-23 14:22:47 -0400

    Part 2 for Parstagram

---
+++ app/src/main/java/com/example/parstagram/PostsAdapter.java
@@ -0,0 +1,82 @@
+package com.example.parstagram;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Post> posts;
+
+    public PostsAdapter(Context context, List<Post> posts){
+        this.context = context;
+        this.posts = posts;
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        posts.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Post> list) {
+        posts.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvUsername;
+        private TextView tvDescription;
+        private ImageView ivImage;
+
+        public ViewHolder (@NonNull View itemView){
+            super(itemView);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+            ivImage = itemView.findViewById(R.id.ivImage);
+        }
+
+        public void bind(Post post) {
+            // Bind the post data to the view elements
+            tvDescription.setText(post.getDescription());
+            tvUsername.setText(post.getUser().getUsername());
+            ParseFile image = post.getImage();
+            if (image != null){
+                Glide.with(context).load(image.getUrl()).into(ivImage);
+            }
+        }
+    }
+}
